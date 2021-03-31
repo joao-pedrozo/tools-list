f4a2cf4f3e84a81a9f1858952b0bd2e2c72d029b
Joao Pedrozo <jp.pedrozo@hotmail.com>
2021-03-31 20:49:52 -0300

    add basic project structure with typescript and styled-components

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import GlobalStyles from "./styles/global";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>teste</h1>
+    </div>
+  );
+}
